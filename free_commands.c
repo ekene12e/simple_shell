8cff75a37413ae65707ead35f21c36899648bc06
ekene12e <ekenestanekeson@gmail.com>
2022-05-18 20:42:56 +0100

    created helper functions

---
+++ free_commands.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * free_cmds - rees the cmd array
+ * @str: pointer to an array of command line
+ * arguments
+ *
+ * Return: void
+ */
+void free_cmds(char **str)
+{
+	int index = 0;
+
+	if (str == NULL)
+		return;
+	while (str[index])
+	{
+		free(str[index]);
+		index++;
+	}
+	free(str);
+}
